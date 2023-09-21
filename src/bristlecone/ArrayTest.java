e9e465bcf793f958a2c2a1c1134fd13f9b82027a
lax <codewithlax@gmail.com>
2023-09-21 18:05:10 +0530

    Bristlecone questions

---
+++ src/bristlecone/ArrayTest.java
@@ -0,0 +1,25 @@
+package src.bristlecone;
+
+//1
+/*["app1","App1","app2","app3","aPp3"]
+ Remove the duplicates irrespective*/
+
+//2
+//   Input     [ApP1, app1, ApP2, ApP3,app3]
+//   Output     [ApP1,ApP2,ApP3]
+
+//3
+/*Input array   =  [0, 1, 0, 1, 0, 0, 1, 1, 1, 0]
+ Output array =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]*/
+
+//4
+//[1,2,3,4,5,6,7,8,9,10,11,13,14,15,17,18]
+// //missing number = 12,16 (output)
+
+
+//5
+//Reverse order
+//1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 17, 18
+public class ArrayTest {
+    //TODO
+}
