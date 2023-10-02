097d6bbfaf246bbea4ff65354b6190a93c8c5c4d
lax <codewithlax@gmail.com>
2023-10-02 17:45:47 +0530

    wissen

--- src/wissen/FlattenNestedArray.java
+++ src/wissen/FlattenNestedArray.java
@@ -4,6 +4,7 @@ package src.wissen;
  * Object[] array = {1, 2, new Object[]{3, 4, new Object[]{5}, 6, 7}, 8, 9, 10};
  * Output = {1,2,3,4,5,6,7,8,9,10}
  */
+
 public class FlattenNestedArray {
     public static void main(String[] args) throws Exception {
         Object[] inputArray = {1, 2, new Object[]{3, 4, new Object[]{5}, 6, 7}, 8, 9, 10};
