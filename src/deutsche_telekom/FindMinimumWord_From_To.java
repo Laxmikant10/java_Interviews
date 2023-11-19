b04dd57d99da246e28e0c479b1a174ca46f99444
lax <codewithlax@gmail.com>
2023-11-19 23:06:31 +0530

    deutsche_telekom

---
+++ src/deutsche_telekom/FindMinimumWord_From_To.java
@@ -0,0 +1,16 @@
+package src.deutsche_telekom;
+
+/**
+ from: "hat"
+ to: "mok"
+ options: [rit, mot, mug, hot, art, cot, rat, mit, mok, cat, ham]
+
+ possible solution1: hat -> rat -> rit -> mit -> mot -> mok
+ possible solution2: hat -> cat -> cot -> mot -> mok
+ possible solution3: hat -> hot > mot -> mok
+
+ O/p: 4
+ */
+public class FindMinimumWord_From_To {
+    //TODO
+}
